882c73ebfcdf55d120443552bb65c3966168060f
Dmitrii <eurosd@yandex.ru>
2020-04-14 13:39:38 +0300

    fixed linter errors

--- src/greeting.js
+++ src/greeting.js
@@ -1,8 +1,6 @@
-import readlineSync from 'readline-sync';
 import brainEven from './games/even.js';
 
-
 export default () => {
-  console.log(`Welcome to the Brain Games!\n`);
-      brainEven();
+  console.log('Welcome to the Brain Games!\n');
+  brainEven();
 };
