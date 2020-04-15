b4d7b50044c46b4ca4210ed77cd98ffc7f4794cb
Dmitrii <eurosd@yandex.ru>
2020-04-15 12:53:34 +0300

    removed the function that returns  a random number. imported 'randomnum' from 'random.js'

--- src/games/even.js
+++ src/games/even.js
@@ -1,9 +1,10 @@
 import startGame from '..';
+import randomnum from '../random.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (num) => num % 2 === 0;
 const getAnswerAndQuestion = () => {
-  const question = Math.floor(Math.random() * 101) + 1;
+  const question = randomnum(0, 101);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
