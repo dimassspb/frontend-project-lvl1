c72888f2061871a4d18a971578bc9a517cd1e9d9
Dmitrii <eurosd@yandex.ru>
2020-04-15 16:32:12 +0300

    added code

---
+++ src/bin/brain-calculator.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import runGame from '../games/calculator.js';
+
+runGame();
---
+++ src/games/calculator.js
@@ -0,0 +1,29 @@
+import startGame from '..';
+import randomnum from '../random.js';
+
+const calculate = (num1, num2, sign) => {
+  switch (sign) {
+    case '-':
+      return num1 - num2;
+    case '+':
+      return num1 + num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return null;
+  }
+};
+
+const description = 'What is the result of the expression?.';
+const signs = ['+', '-', '*'];
+
+const getAnswerAndQuestion = () => {
+  const randomsignindex = Math.floor(Math.random() * signs.length);
+  const randomSign = signs[randomsignindex];
+  const num1 = randomnum(0, 101);
+  const num2 = randomnum(0, 101);
+  const correctAnswer = String(calculate(num1, num2, randomSign));
+  const question = `${num1} ${randomSign} ${num2}`;
+  return [question, correctAnswer];
+};
+export default () => startGame(description, getAnswerAndQuestion);
