d7b03f8cb67716639d6276dc4653a90efb8b9f98
Dmitrii <eurosd@yandex.ru>
2020-04-24 15:19:48 +0300

    added code

---
+++ src/games/prime.js
@@ -0,0 +1,22 @@
+import startGame from '..';
+import randomnum from '../random.js';
+
+const isPrime = (num) => {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const getData = () => {
+  const question = randomnum(0, 11);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+export default () => startGame(description, getData);
+
