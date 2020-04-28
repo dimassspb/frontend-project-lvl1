0da8fbf65f0a6a71652e074fcd2a3443906f3e19
Dmitrii <eurosd@yandex.ru>
2020-04-28 10:31:42 +0300

    corrected the greeting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!\n');
+console.log('Welcome to the Brain Games!');
 const roundsCount = 3;
 const runGame = (description, getData) => {
   const name = readlineSync.question('May I have your name?:');
