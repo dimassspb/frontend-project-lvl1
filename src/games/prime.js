c4b0cc89e3c5e51cfdf02fe741268076d1f6aa26
Dmitrii <eurosd@yandex.ru>
2020-04-29 19:15:59 +0300

    corrected

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,10 +2,10 @@ import runGame from '../index.js';
 import random from '../random.js';
 
 const isPrime = (num) => {
-  if (num <= 1) {
+  if (num < 2) {
     return false;
   }
-  for (let i = 2; i < num; i += 1) {
+  for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
     }
